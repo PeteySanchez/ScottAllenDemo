bd3c4ac59a832127b5bcea9d6b114b4607d98ab6
PeteySanchez <37543322+PeteySanchez@users.noreply.github.com>
2018-08-01 16:37:30 +0100

    Fix stuff

--- ScottAllenDemo/App_Start/RouteConfig.cs
+++ ScottAllenDemo/App_Start/RouteConfig.cs
@@ -13,11 +13,6 @@ namespace ScottAllenDemo
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            // /Cuisine/french
-            routes.MapRoute("Cuisine",
-                "cuisine/{name}",
-                new { Controller = "Cuisine", action = "Search", name = UrlParameter.Optional });
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
